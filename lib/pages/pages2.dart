678f98f3c9433bfb226ae50eda8dd73a489eaedd
David Pereda Rentero <davidpereda@ceroca.cat>
2025-10-21 16:17:03 +0200

    Creacion Pagina 2(Menu Inicio)

---
+++ lib/pages/pages2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Pages2 extends StatefulWidget {
+  const Pages2({super.key});
+
+  @override
+  State<Pages2> createState() => _Pages2State();
+}
+
+class _Pages2State extends State<Pages2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("Bienvenido")
+        ],
+      ),
+    );
+  }
+}
