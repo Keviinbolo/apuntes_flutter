a842017065b04dbdf62568ef8c7a686ed571ac47
Kevin Yamel Díaz Pérez <kevindiaz@ceroca.cat>
2025-10-21 16:05:43 +0200

    Creación de la pagina1 (Registro de usuario)

--- lib/pages/pages1.dart
+++ lib/pages/pages1.dart
@@ -5,6 +5,8 @@ class Pages1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+    );
   }
 }
