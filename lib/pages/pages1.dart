671c99c3d2c1ee61f64076114e282f47a26d1462
Kevin Yamel Díaz Pérez <kevindiaz@ceroca.cat>
2025-10-21 16:29:39 +0200

    Pagina 1 (Registro)
    Agregado dos Textfield, para Nombre y Correo

--- lib/pages/pages1.dart
+++ lib/pages/pages1.dart
@@ -1,12 +1,43 @@
+import 'package:apuntes_flutter/class/textfield_personalizado.dart';
 import 'package:flutter/material.dart';
 
-class Pages1 extends StatelessWidget {
+class Pages1 extends StatefulWidget {
   const Pages1({super.key});
 
   @override
+  State<Pages1> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Pages1> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[600],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  "Registro",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Column(
+                  children: [
+                    TextfieldPersonalizado(valorCampo: TextEditingController(), hintText: "Introduce tu nombre",),
+                    SizedBox(width: 10,),
+                    TextfieldPersonalizado(valorCampo: TextEditingController(), labelname: "Correo Electrónico", hintText: "Introduce tu correo electrónico",),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
